33f9a8aa7c803b923bc98769df2d0dab0806d0f1
FHallengreen <fred70p9@stud.kea.dk>
2022-08-08 13:32:33 +0200

    can now add int

--- src/FreddyArrayList.java
+++ src/FreddyArrayList.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
 
 public class FreddyArrayList {
 
     // Class Object is the root of the class hierarchy. Every class has Object as a superclass. All objects, including arrays, implement the methods of this class.
     // Object[] stores the objects in an array. In this case we use int.
-    private Object[] freddyArrayList;
+    private int[] freddyArrayList;
     private int elementsInFreddyArray;
+    private static final int DEFAULT_CAPACITY = 10;
 
     public FreddyArrayList() {
-        this(10);
+        this(DEFAULT_CAPACITY);
     }
 
     // Only if i is greater than 0, can below Contructor be initialized with i as the size and elements = 0.
@@ -16,19 +18,53 @@ public class FreddyArrayList {
         if (i <= 0) {
             System.out.println("Size cannot be less than 0");
         } else {
-            this.freddyArrayList = new Object[i];
+            this.freddyArrayList = new int[i];
             this.elementsInFreddyArray = 0;
         }
+    }
+
+    //checks if array length matches elements in array.
+    private boolean isArrayFull() {
+        return this.freddyArrayList.length == this.elementsInFreddyArray;
     }
 
     public int size() {
         return this.elementsInFreddyArray;
     }
 
+    public int[] arraySize() {
+        return this.freddyArrayList;
+    }
+
+    // adds an int to the array[i] position. If value on position is 0, it will overwrite value and add one to the elements and afterwards break out the loop.
+    public void addInt(int value) {
+        for (int i = 0; i < freddyArrayList.length; i++) {
+            if (get(i) == 0) {
+                this.freddyArrayList[this.elementsInFreddyArray] = value;
+                this.elementsInFreddyArray++;
+                break;
+            }
+        }
+    }
+
+    // takes the index from the for loop and assigns value with it's value. It returns the value where get is called.
+    public int get(int index) {
+        int value = 0;
+        try {
+            value = this.freddyArrayList[index];
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+            System.exit(-1);
+        }
+        return value;
+    }
 
     public static void main(String[] args) {
 
         FreddyArrayList test = new FreddyArrayList();
         System.out.println(test.size());
+        System.out.println(Arrays.toString(test.arraySize()));
+        test.addInt(39);
+        System.out.println(Arrays.toString(test.arraySize()));
     }
 }
