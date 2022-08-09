c420438d4a11c2d99e942361c3e619d8b5cebc46
FHallengreen <fred70p9@stud.kea.dk>
2022-08-09 11:58:01 +0200

    remove from array

--- src/FreddyArrayList.java
+++ src/FreddyArrayList.java
@@ -7,6 +7,7 @@ public class FreddyArrayList {
     private int[] freddyArrayList;
     private int elementsInFreddyArray;
     private static final int DEFAULT_CAPACITY = 2;
+    int tempPosition = -1;
 
     public FreddyArrayList() {
         this(DEFAULT_CAPACITY);
@@ -25,7 +26,10 @@ public class FreddyArrayList {
 
     //checks if array length matches elements in array.
     private boolean isArrayFull() {
-        return this.freddyArrayList.length == this.elementsInFreddyArray;
+
+        if (this.elementsInFreddyArray == this.freddyArrayList.length) {
+            return true;
+        } else return false;
     }
 
     public int size() {
@@ -38,17 +42,21 @@ public class FreddyArrayList {
 
     // adds an int to the array[i] position. If value on position is 0, it will overwrite value and add one to the elements and afterwards break out the loop.
     public void addInt(int value) {
-             if (isArrayFull()){
-                 increaseArray();
-                 System.out.println("Array is full, cannot add entered value: " + value);
-            }
-                this.freddyArrayList[this.elementsInFreddyArray] = value;
-                this.elementsInFreddyArray++;
+        if (isArrayFull()) {
+            increaseArray();
+        }
+        if (tempPosition != -1) {
+            this.freddyArrayList[tempPosition] = value;
+            this.elementsInFreddyArray++;
+            tempPosition = -1;
+        } else {
+            this.freddyArrayList[this.elementsInFreddyArray] = value;
+            this.elementsInFreddyArray++;
+        }
     }
 
-    // TODO: increase array size.
     private void increaseArray() {
-        int [] newFreddyArray = new int [freddyArrayList.length * 2];
+        int[] newFreddyArray = new int[freddyArrayList.length * 2];
 
         for (int i = 0; i < freddyArrayList.length; i++) {
             newFreddyArray[i] = freddyArrayList[i];
@@ -57,8 +65,15 @@ public class FreddyArrayList {
         freddyArrayList = newFreddyArray;
     }
 
-    // TODO: remove element from the array.
-
+    public void removeFromArray(int value) {
+        for (int i = 0; i < freddyArrayList.length; i++) {
+            if (value == freddyArrayList[i]) {
+                freddyArrayList[i] = 0;
+                tempPosition = freddyArrayList[get(i)];
+                this.elementsInFreddyArray--;
+            }
+        }
+    }
 
     // takes the index from the for loop and assigns value with it's value. It returns the value where get is called.
     public int get(int index) {
@@ -80,6 +95,10 @@ public class FreddyArrayList {
         test.addInt(39);
         test.addInt(22);
         test.addInt(11);
+        test.removeFromArray(39);
+        test.addInt(35);
+        test.addInt(34);
+        test.addInt(23);
         System.out.println(Arrays.toString(test.array()));
     }
 }
